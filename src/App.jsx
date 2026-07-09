c1d2ddc807cb1aa13ebb0ea92996addcf7990b9c
adriarodr <158720918+adriarodr@users.noreply.github.com>
2026-07-09 15:14:24 -0400

    chore: init

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import TodoList from './TodoList';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <h1>React API Practice</h1>
+
+      <p>
+        This app connects to a practice API, retrieves todo data, and displays
+        the results using React.
+      </p>
+
+      <TodoList />
+    </main>
+  );
+}
+
+export default App;
